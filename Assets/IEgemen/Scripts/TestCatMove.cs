a3d5520e40cb356ae3bdddff3f71e6bc39603412
Egemen Azay <egemenazay39@gmail.com>
2024-07-23 21:30:59 +0300

    driftli kedi animasyonları eklendi koşması kaldı bi tek

--- Assets/IEgemen/Scripts/TestCatMove.cs
+++ Assets/IEgemen/Scripts/TestCatMove.cs
@@ -3,88 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class CatMovement : MonoBehaviour
+public class TestCatMove : MonoBehaviour
 {
     public Camera playerCamera;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
     public float jumpPower = 7f;
     public float gravity = 10f;
-    
-
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
 
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private CharacterController characterController;
+    public Animator animator;
+    
+    private bool canMove = true;
 
-    Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
-
-    public bool canMove = true;
-    
-    CharacterController characterController;
-    
-    //updateler
-    public Transform groundCheck;
-    private Animator animator;
-    public float groundCheckRadius = 0.1f;
-    public LayerMask groundLayer;
-    private bool isGrounded;
-    private Rigidbody rb;
-    
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-
-        #region Handles Movment
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        if (isRunning)
-        {
-            animator.SetBool("isRuning",true);
-        }
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-        #endregion
-
-        #region Handles Jumping
-        /*if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        if (curSpeedX != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (curSpeedX != 0 && isRunning)
+        {
+            animator.SetBool("isRuning", true);
+        }
+        else
+        {
+            animator.SetBool("isRuning", false);
+            animator.SetBool("isMoving", false);
+        }
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
-            animator.SetBool("onAir", true);
         }
         else
         {
             moveDirection.y = movementDirectionY;
         }
-
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
-        }*/
-        CheckGroundStatus();
-        Debug.Log(isGrounded);
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
+            animator.SetBool("onAir", true);
         }
-        
-        #endregion
+        else
+        {
+            animator.SetBool("onAir", false);
+        }
 
-        #region Handles Rotation
+
         characterController.Move(moveDirection * Time.deltaTime);
 
         if (canMove)
@@ -95,30 +79,6 @@ public class CatMovement : MonoBehaviour
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
 
-        #endregion
     }
-    void CheckGroundStatus()
-    {
-        if (groundCheck != null)
-        {
-            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
-        }
-        else
-        {
-            isGrounded = false;
-        }
-    }
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        }
-    }
-    void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-    }
-    
 }
+
